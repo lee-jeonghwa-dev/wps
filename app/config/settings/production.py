cab1d077da48cb6410362fb606644a4e2ef27d19
chajeehyung <chajee308@gmail.com>
2018-11-21 18:18:53 +0900

    database 설정 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,2 +1,9 @@
 from .base import *
 DEBUG = False
+
+secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = secrets['DATABASE']
