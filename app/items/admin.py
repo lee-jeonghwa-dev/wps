bea363268ac0612bf29d927f91a8793b51c5caa0
lee-jeonghwa <lee.jeonghwa02@gmail.com>
2018-11-26 17:54:02 +0900

    Description 모델 추가

--- app/items/admin.py
+++ app/items/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Item, Category, ItemImage
+from .models import Item, Category, ItemImage, Description
 
 admin.site.register(Item)
 admin.site.register(Category)
 admin.site.register(ItemImage)
+admin.site.register(Description)
