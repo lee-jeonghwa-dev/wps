667ff05b39dbab97471d6342df8457a9bb3a3d4e
lee-jeonghwa <lee.jeonghwa02@gmail.com>
2018-12-10 11:58:27 +0900

    django-debug-toolbar dev에 설정

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from config import settings
 from items import apis as items_apis
 from members import apis as members_apis
 from bill import apis as cart_apis
@@ -61,3 +63,8 @@ urlpatterns = [
     path('', include(urlpatterns_api)),
 ]
 
+if settings.dev.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
